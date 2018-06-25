8daf4a6cadeb3b39b76cba274755daaf8bbdff20
Jakub Wiśniewski <jwisniewski@blastlab.co>
2018-06-25 12:37:46 +0200

    created initial empty spring boot project with swagger-ui

---
+++ src/main/java/co/blastlab/cityhack/Application.java
@@ -0,0 +1,20 @@
+package co.blastlab.cityhack;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	@SuppressWarnings("unused")
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
